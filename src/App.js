e60706617bc4c1d27bfc68bb84699f3316f2492e
Sapana Kumari Singh <rktsapana@gmail.com>
2023-09-07 10:01:08 +0530

    07/09/2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AttendanceCalendar from './AttendanceCalendar ';
+
+
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <AttendanceCalendar />
+</div>
+        );
 }
 
-export default App;
+        export default App;
---
+++ src/AttendanceCalendar .jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Modal from 'react-modal';
+import './AttendanceCalendar.css';
+
+const AttendanceCalendar  = () => {
+
+  const [date, setDate] = useState(new Date());
+  const [attendance, setAttendance] = useState({});
+  const [isOpen, setOpen] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/attendance')
+      .then((response) => response.json())
+      .then((data) => setAttendance(data))
+      .catch((error) => console.error('Error loading attendance data:', error));
+  }, []);
+
+  const handleDateClick = (clickedDate) => {
+    setDate(clickedDate);
+    setOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setOpen(false);
+  };
+
+  const handleAttendanceChange = (status) => {
+    const newAttendance = { ...attendance };
+    newAttendance[date.toISOString()] = status;
+    setAttendance(newAttendance);
+
+    fetch('http://localhost:3001/api/attendance', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ date: date.toISOString(), status }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message);
+      })
+      .catch((error) => {
+        console.error('Error storing attendance data:', error);
+      });
+
+      setOpen(false);
+  };
+
+  const tileClassName = ({ date }) => {
+    const dateKey = date.toISOString();
+    if (attendance[dateKey] === 'present') {
+      return 'present';
+    } else if (attendance[dateKey] === 'absent') {
+      return 'absent';
+    }
+    return '';
+  };
+
+
+  return (
+    <div>   <h2>Attendance Calendar</h2>
+    <Calendar
+      onClickDay={handleDateClick}
+      value={date}
+      tileClassName={tileClassName}
+    />
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleModalClose}
+      contentLabel="Attendance Modal"
+    >
+      <h3>Date: {date.toDateString()}</h3>
+      <button
+        className="modal-button present"
+        onClick={() => handleAttendanceChange('present')}
+      >
+        Present
+      </button>
+      <button
+        className="modal-button absent"
+        onClick={() => handleAttendanceChange('absent')}
+      >
+        Absent
+      </button>
+    </Modal>
+     </div>
+  )
+}
+
+export default AttendanceCalendar 
